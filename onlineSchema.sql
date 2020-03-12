705192a4bb51f5d0763fcb662cf657de9dba69ed
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-03-11 20:12:04 -0400

    restocking function working

---
+++ onlineSchema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS Storefront_DB;
+CREATE DATABASE Storefront_DB;
+USE Storefront_DB;
+
+CREATE TABLE SalesTable
+(
+    Product_ID INTEGER,
+    Category VARCHAR (40),
+    Items_For_Sale VARCHAR (100),
+    Price_Per_Item DECIMAL (10,3),
+    Quantity_Available DECIMAL (10,1),
+    Discount DECIMAL(10,3),
+    Items_Sold DECIMAL(10,3),
+    Total_Sales DECIMAL (10,3),
+    PRIMARY KEY (Product_ID)
+);
--- store.js
+++ store.js
@@ -1,73 +1,107 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "Storefront_db"
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    introductions()
+
+});
+
+
+
 function introductions() {
     inquirer.prompt({
 
         name: "introductions",
         type: "list",
-        message: "\n\nWelcome to Matt Krebs's Store! \n\n\n Are you a store manager or a customer?",
-        choices: ["Manager", "Customer", "Exit"]
+        message: "\n\nWelcome to our Online Office Store! \n\n\n Are you a Store Manager, a Customer or a Supervisor?\n\n\n",
+        choices: ["Manager", "Customer", "Supervisor", "Exit"]
 
     }
 
     )
         .then(function (answer) {
 
-            if (answer.introductions === "Customer") {
+            switch (answer.introductions) {
+                case "Manager":
+                    console.log("\n\nThank you for checking in today!\n");
+                    managerOptions();
+                    break;
 
+                case "Customer":
+                    console.log("\n\nThank You for Visiting us today!\n");
 
+                    customerOptions();
+                    // customerDocumentation();
+                    break;
 
-                console.log("\n\nThank You for Visiting us today!\n");
+                case "Supervisor":
+                    console.log("\n\nThank you for checking in today!\n");
+                    // supervisorOptions();
+                    break;
 
-                customerDocumentation()
-
-
-
+                case "Exit":
+                    connection.end();
+                    break;
             }
-            else if (answer.introductions === "Manager") {
 
-                console.log("\n\nThank you for checking in today!\n");
-
-                managerOptions();
-
-            }
-            else {
-
-                console.log("\n\nThanks for coming and have a nice day!");
-
-            }
-        })
+        });
 }
+
+
+
+
+
+
 
 function managerOptions() {
     inquirer.prompt({
-
         name: "managerOptions",
         type: "list",
-        message: "\n\nFYI - A new shipment of inventory has just arrived. \n\nWhat would you like to do right now?",
-        choices: ["Review Current Inventory", "Accept shipment - Stock Inventory", "Check Sales"]
+        message: "\n\nWhat would you like to do right now?",
+        choices: ["Review Current Inventory", "Consider Restocking", "Check Sales", "Go Home"]
     })
 
         .then(function (answer) {
-            if (answer.managerOptions === "Review Current Inventory") {
 
-                console.log("\n\nOK lets take a look to see what is on our shelves!\n\n")
+            switch (answer.managerOptions) {
+                case "Review Current Inventory":
+                    console.log("\n\nOK lets take a look to see what is on our shelves!\n\n");
+                    seeWhatsOnTheShelves();
+                    break;
 
-                // readCurrentInventory()
+                case "Consider Restocking":
 
-            } else if (answer.managerOptions === "Accept shipment - Stock Inventory") {
+                    checkStock();
+                    break;
 
-                console.log("\n\nOK Great - We have just restocked the shelves with this shipment. \n\n It included x units of this \n\ny units of that \n\nand z units of that\n\n");
 
+                case "Check Sales":
+                    console.log("\n\nYour sales are as follows numbers\n\n");
+                    break;
 
-            } else {
-
-                console.log("\n\nYour sales are as follows numbers\n\n");
+                case "Go Home":
+                    console.log("\n\nYou just got here!!")
+                    connection.end;
 
             }
-
-        })
+        });
 }
 
 
@@ -77,127 +111,338 @@ function customerOptions() {
         name: "customerOptions",
         type: "list",
         message: "\n\nWhat would you like to do right now?",
-        choices: ["See your purchase history", "Go Shopping","Go Home"]
+        choices: ["See your purchase history", "Go Shopping", "Go Home"]
     })
 
         .then(function (answer) {
-            if (answer.customerOptions === "Review Your Purchase History") {
+            switch (answer.customerOptions) {
 
-                console.log("\n\nOK lets take a look to see what you have purchased so far!\n\n")
+                case "See your purchase history":
+                    console.log("\n\nOK lets take a look to see what you have purchased so far!\n\n");
+                    break;
 
-                // recordOfPurchases()
+                case "Go Shopping":
+                    console.log("\n\nOK Let's go shopping!\n\n");
+                    seeWhatsOnTheShelves();
+                    break;
 
-            } else if (answer.customerOptions === "Go Shopping") 
-            
-            {
+                case "Go Home":
+                    console.log("\n\nCome back soon!\n\n");
+                    connection.end;
+                    break;
 
-
-                itemsDesired();
-
-            } else {
-
-               // connection.end();
             }
-        })
+        });
 }
 
+function seeWhatsOnTheShelves() {
 
-introductions()
+    var query = "SELECT Category, Items_For_Sale, Price_Per_Item, Quantity_Available FROM SalesTable";
+    console.log("\x1b[44m%s\x1b[0m", "Items For Sale");
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
 
 
-function customerDocumentation() {
-
-    inquirer.prompt([
-        {
-            name: "nameOfVisitor",
-            type: "input",
-            message: "please enter your name"
-        },
-        {
-            name: "addressOfVisitor",
-            type: "input",
-            message: "please enter your address"
+            console.log("\x1b[44m%s\x1b[0m", "\n" + res[i].Items_For_Sale + "        Price: $" + res[i].Price_Per_Item + "        Quantity Available: " + res[i].Quantity_Available);
         }
-    ])
-        .then(function (answer) {
-
-            //check information against users logged in mysql
-
-            if (answer.nameOfVisitor === "a name in customer list" && answer.addressOfVisitor === "") {
-                console.log("\n\nWelcome Back!\n\n")
-
-                customerOptions()
-
-            } else {
-
-                //log information into users file in mysql
-
-                console.log("\n\nWe see that your address and name is not an exact match.\n\n We will add your information to our files\n\n")
-
-                customerOptions()
-            }
 
 
 
-        })
+    });
 
+    managerOptions()
 
 }
 
-function itemsDesired() {
-    inquirer.prompt([{
+//                 itemsDesired();
 
-        name: "itemsDesired",
-        type: "list",
-        message: "\n\n At Matt Krebs's store we have a fine selection of products!\n\n What were you looking for today?",
-        choices: ["Umbrellas", "Hampsters", "Machine Guns"]
-    },
-    {
-        name: "quantityDesired",
-        input: "number",
-        message: "\n\n How many of these were you looking to purchase today \n\n(Must not be greater than ______ )\n\n"
-    }
 
-    ])
-        .then(function (answer) {
-            if (answer.quantityDesired > 0 && answer.quantityDesired <= 6)  //make variable out of inventory of desired number and compare)
-            {
-                console.log("\n\nyou have chosen to buy 7 _____ at $_____\n\n")
-                payment()
-            } else {
 
-                console.log("\n\nWe only have ___ left in stock.\n\n Please pick a number that is greater than zero but less than\n\n")
-
-            }
-
-        })
-}
-
-function payment() {
-
-console.log("your order is to buy x of x at x number");
+function checkStock() {
 
     inquirer.prompt({
-
-        name: "confirmOrder",
+        name: "howManyItemsOnShelves",
         type: "list",
-        message: "Would you like to go forward with this order",
-        choices: ["Yes", "no"]
+        message: "\n\nEach Manager is different.\n\nAs items are flying off the shelves (due to sales) -\n\nAt what number (quantity) should each item be at before you decide to restock\n\n",
+        choices: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     })
 
         .then(function (answer) {
-            if (answer.confirmOrder === "Yes"){
-            console.log("Great we have now magically taken x from you and in 10-15 business days you will receive your _______. Your order has been documented in purchase history.")
+            var threshold = answer.howManyItemsOnShelves;
 
-            //document purchase - in purchase history - in inventory - in sales. 
+            console.log(threshold);
 
-            } else {
+            console.log("\n\nOK lets check to see if any of our items are below that threshold and need to be restocked.\n\n");
 
-                console.log("Sorry that you have decided not to purchase the_______. They are very popular. For now we will return you to the main menu ")
+            var query = "SELECT Quantity_Available FROM SalesTable";
 
-            }
+            connection.query(query, function (err, res) {
+                if (err) throw err;
+
+                for (var i = 0; i < res.length; i++) {
+
+                    if (res[i].Quantity_Available < threshold) {
+
+                        console.log("OK based on the threshold that you haves set you are going to need to restock.");
+
+                        inquirer.prompt({
+                            name: "maximumAmountOnShelves",
+                            type: "list",
+                            message: "\n\n In terms of quantity of Items (in stock) what is your limit on the high end?",
+                            choices: [14, 15, 16, 17, 18, 19, 20]
+                        })
+
+                            .then(function (answer) {
+                                var highthreshold = answer.maximumAmountOnShelves;
+                               
+
+
+                                console.log(highthreshold);
+
+                                console.log("\n\nOK so we won't go over " + highthreshold + ".");
+
+                                var nextquery = "UPDATE SalesTable SET Quantity_Available = " + highthreshold + " WHERE Quantity_Available < " + threshold;
+
+                                // RESET
+                                // UPDATE SalesTable SET Quantity_Available = 4 Where Quantity_Available > 8
+
+                                connection.query(nextquery, function (err, result) {
+                                    if (err) throw err;
+
+                                  
+
+                                    console.log("\n\nOK you've restocked those items that needed it.\n\n")
+                                   
+                                    managerOptions();
+                                })
 
 
 
-})
+                            })
+                        return false;
+                    } else {
+
+                    }
+                }
+                console.log("OK based on the threshold that you have set we currently have plenty on the shelves. ")
+            })
+
+
+
+
+
+        })
+
+
+
+
+
+
+
+
+
+    // managerOptions();
+
 }
+
+
+
+
+
+
+
+// function customerDocumentation() {
+
+//     inquirer.prompt([
+//         {
+//             name: "nameOfVisitor",
+//             type: "input",
+//             message: "please enter your name"
+//         },
+//         {
+//             name: "addressOfVisitor",
+//             type: "input",
+//             message: "please enter your address"
+//         }
+//     ])
+//         .then(function (answer) {
+
+//             //check information against users logged in mysql
+
+//             if (answer.nameOfVisitor === "a name in customer list" && answer.addressOfVisitor === "") {
+//                 console.log("\n\nWelcome Back!\n\n")
+
+//                 customerOptions()
+
+//             } else {
+
+//                 //log information into users file in mysql
+
+//                 console.log("\n\nWe see that your address and name is not an exact match.\n\n We will add your information to our files\n\n")
+
+//                 customerOptions()
+//             }
+
+
+
+//         })
+
+
+// }
+
+// function itemsDesired() {
+//     inquirer.prompt([{
+
+//         name: "itemsDesired",
+//         type: "list",
+//         message: "\n\n At Matt Krebs's store we have a fine selection of products!\n\n What were you looking for today?",
+//         choices: ["Umbrellas", "Hampsters", "Machine Guns"]
+//     },
+//     {
+//         name: "quantityDesired",
+//         input: "number",
+//         message: "\n\n How many of these were you looking to purchase today \n\n(Must not be greater than ______ )\n\n"
+//     }
+
+//     ])
+//         .then(function (answer) {
+//             if (answer.quantityDesired > 0 && answer.quantityDesired <= 6)  //make variable out of inventory of desired number and compare)
+//             {
+//                 console.log("\n\nyou have chosen to buy 7 _____ at $_____\n\n")
+//                 payment()
+//             } else {
+
+//                 console.log("\n\nWe only have ___ left in stock.\n\n Please pick a number that is greater than zero but less than\n\n")
+
+//             }
+
+//         })
+// }
+
+// function payment() {
+
+// console.log("your order is to buy x of x at x number");
+
+//     inquirer.prompt({
+
+//         name: "confirmOrder",
+//         type: "list",
+//         message: "Would you like to go forward with this order",
+//         choices: ["Yes", "no"]
+//     })
+
+//         .then(function (answer) {
+//             if (answer.confirmOrder === "Yes"){
+//             console.log("Great we have now magically taken x from you and in 10-15 business days you will receive your _______. Your order has been documented in purchase history.")
+
+//             //document purchase - in purchase history - in inventory - in sales. 
+
+//             } else {
+
+//                 console.log("Sorry that you have decided not to purchase the_______. They are very popular. For now we will return you to the main menu ")
+
+//             }
+
+
+
+// })
+// }
+
+// function restock(){
+
+// UPDATE SalesTable
+// SET Quantity_Available = highthreshold
+// WHERE Quantity_Available < threshold
+
+// }
+
+function maximumAmount() {
+    inquirer.prompt({
+        name: "maximumAmountOnShelves",
+        type: "list",
+        message: "\n\n In terms of quantity of Items (in stock) what is your limit on the high end?",
+        choices: [14, 15, 16, 17, 18, 19, 20]
+    })
+
+        .then(function (answer) {
+            var highthreshold = answer.maximumAmountOnShelves;
+
+
+            console.log(highthreshold);
+
+            console.log("\n\nOK lets check to see if any of our items \n\n are below that threshold and need to be restocked");
+
+
+
+        })
+}
+
+
+
+// function checkStock() {
+
+//     inquirer.prompt({
+//         name: "howManyItemsOnShelves",
+//         type: "list",
+//         message: "\n\nEach Manager is different.\n\nAs items are flying off the shelves (due to sales) -\n\nAt what number (quantity) should each item be at before you decide to restock\n\n",
+//         choices: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+//     })
+
+//         .then(function (answer) {
+//             var threshold = answer.howManyItemsOnShelves;
+
+//             console.log(threshold);
+
+//             console.log("\n\nOK lets check to see if any of our items are below that threshold and need to be restocked.\n\n");
+
+//             var query = "SELECT Quantity_Available FROM SalesTable";
+
+//             connection.query(query, function (err, res) {
+//                 if (err) throw err;
+
+//                 for (var i = 0; i < res.length; i++) {
+
+//                     if (res[i].Quantity_Available < threshold) {
+
+//                         console.log("OK based on the threshold that you haves set you are going to need to restock.");
+
+//                         inquirer.prompt({
+//                             name: "maximumAmountOnShelves",
+//                             type: "list",
+//                             message: "\n\n In terms of quantity of Items (in stock) what is your limit on the high end?",
+//                             choices: [14, 15, 16, 17, 18, 19, 20]
+//                         })
+
+//                             .then(function (answer) {
+//                                 var highthreshold = answer.maximumAmountOnShelves;
+
+
+
+//                                 console.log(highthreshold);
+
+//                                 console.log("\n\nOK so we won't go over " + highthreshold + ".");
+
+
+
+//                                 var nextquery = "UPDATE SalesTable SET Quantity_Available = " + highthreshold + "WHERE Quantity_Available < " + threshold;
+
+//                                 connection.query(nextquery, function (err, result) {
+//                                     if (err) throw err;
+
+//                                     console.log(result);
+//                                  })
+
+
+
+//                                 })
+//                                 return false;
+//                             } else {
+
+//                             }
+                
+//                 console.log("OK based on the threshold that you have set we currently have plenty on the shelves. ")
+         
+//  })
+
